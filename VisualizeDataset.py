5bc1eb2d59c427e3c8e3fde375ead76f2258d7b1
Kote <kote.gotiashvili@gmail.com>
2024-03-24 19:21:14 +0400

    Add - Visualize tranfomed vs real image

--- VisualizeDataset.py
+++ VisualizeDataset.py
@@ -18,6 +18,10 @@ def walk_through_dir(dir_path):
 train_dir = image_path / "train"
 test_dir = image_path / "test"
 
+def get_image_path_list():
+    image_path_list = list(image_path.glob("*/*/*.jpg"))
+
+    return image_path_list
 def get_random_image():
     # get paths */ means get everything from files and last get jpg images
     image_path_list = list(image_path.glob("*/*/*.jpg"))
@@ -56,6 +60,31 @@ def visualize_using_matplotlib():
     plt.title(F" Image class {img[1]}  Shape: {image_as_array.shape} -> Height, Width, Color Channels ")
     plt.show()
 
-visualize_using_matplotlib()
+def plot_tranformed_images(image_paths, tranform, n=5, seed=None):
+    """
+    Selects random images from a path of images and loads/transforms them,
+    then plots the original vs the transformed version, """
+    if seed:
+        random.seed(seed)
+    random_image_paths = random.sample(image_paths, k=n)
+    for image_path in random_image_paths:
+        with Image.open(image_path) as f:
+            fig, ax = plt.subplots(nrows=1, ncols=2) # Do manually, can be used for loop
+            ax[0].imshow(f)
+            ax[0].set_title(f" Original\n Size: {f.size}")
+            ax[0].axis(False)
+
+            # Transform and plot target image (C,H,W) -> (H,W,C)
+            transformed_image = tranform(f).permute(1,2,0) # permute for swap color channel RGD from first to last
+            ax[1].imshow(transformed_image)
+            ax[1].set_title(f" Tranformed \n Shape: {transformed_image.shape}")
+            ax[1].axis(False)
+
+            fig.suptitle(f" Class: {image_path.parent.stem}", fontsize=16)
+            plt.show()
+
+
+
+
 
 
