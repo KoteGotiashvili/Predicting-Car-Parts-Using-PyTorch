4883c9b1d6fa4625f77075c50337c8accd274f54
Kote <kote.gotiashvili@gmail.com>
2024-03-25 18:45:50 +0400

    Add Data Augmentation for Different variations of images, different angles, different colors and so forth

--- CancerClassificationModel.py
+++ CancerClassificationModel.py
@@ -1,8 +1,16 @@
+import os
 import torch
 from torch.utils.data import  DataLoader
 from torchvision import datasets, transforms
 from VisualizeDataset import plot_tranformed_images, get_image_path_list
+from torchvision import datasets
+from pathlib import Path
+from torch.utils.data import DataLoader
 
+image_path = Path("data")
+train_dir = image_path / "train"
+test_dir = image_path / "test"
+BATCH_SIZE = 32
 # Transform data into tensors(numerical representation of images)
 # docs : https://pytorch.org/vision/stable/transforms.html
 # visualized docs: https://pytorch.org/vision/main/auto_examples/transforms/plot_transforms_illustrations.html
@@ -15,7 +23,48 @@ data_transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
+# plot_tranformed_images(image_paths=get_image_path_list(),
+#                        tranform=data_transform,
+#                        n=3,
+#                        seed=None)
+
+# Load image classification data using ImageFolder
+train_data = datasets.ImageFolder(root=train_dir,
+                                  transform=data_transform, # transform for data
+                                  target_transform=None) # transform for label, infered as target directory, do not need some label, if neccessary can pass target labels
+test_data = datasets.ImageFolder(root=test_dir,
+                                  transform=data_transform) # transform for data
+
+# Find out details about transformation and image path
+# print(train_data, test_data)
+
+class_names = train_data.classes # get classes
+class_dict = train_data.class_to_idx # get as dictionary
+
+# Turn loaded images into DataLoaders, turn into iterables, specify batch size
+train_dataloader = DataLoader(dataset=train_data,
+                              batch_size=BATCH_SIZE,
+                              num_workers=os.cpu_count()//2,# How many cpu cors used to load data, all count divide by 2
+                              shuffle=True) # shuffle order
+test_dataloader=DataLoader(dataset=test_data,
+                           batch_size=BATCH_SIZE,
+                           num_workers=os.cpu_count()//2,
+                           shuffle=False) # Order
+# Other forms of transforms -> Data Augmentation -> Change image location, bright, colors, rotation -
+# adding slightly modified copies helps to learn better AKA Data Augmentation
+# Helps view images from different angles, prespectives
+
+train_transform = transforms.Compose([
+    transforms.Resize(size=(224, 224)),
+    transforms.TrivialAugmentWide(num_magnitude_bins=5), # Selects augmentation types and apply some of them randomly on data
+    transforms.ToTensor()
+])
+test_transform = transforms.Compose([
+    transforms.Resize(size=(224, 224)),
+    transforms.ToTensor()
+])
+
 plot_tranformed_images(image_paths=get_image_path_list(),
-                       tranform=data_transform,
+                       tranform=train_transform,
                        n=3,
-                       seed=42)
+                       seed=None)
